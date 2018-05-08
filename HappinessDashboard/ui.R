96e09880c687a3f25f440c7ada5582c8b44ed245
anishsingh@20 <loveeanish@gmail.com>
2018-05-08 11:09:22 +0530

    loaded the datasets in the UI file

--- HappinessDashboard/ui.R
+++ HappinessDashboard/ui.R
@@ -2,4 +2,33 @@ require(shinydashboard)
 require(shiny)
 require(dplyr)
 require(highcharter)
+require(tidyr)
+
+
+
+
+#reading the datasets
+year_2015<-read.csv(file = "2015.csv")
+year_2016<-read.csv(file = "2016.csv")
+year_2017<-read.csv(file = "2017.csv")
+
+
+#function to remove columns with NA values
+remove_col<-function(df,x,colNum)
+{
+  df<-df[-colNum] #exculding column 
+  df #returns a updated dataframe with removed column X
+  
+  #can also use df[col]<-NULL to remove a dataframe
+  
+}
+
+#removing NA values
+year_2015<-remove_col(year_2015,X,13)
+year_2016<-remove_col(year_2016,X,14)
+year_2017<-remove_col(year_2017,X,13)
+
+
+
+
 
